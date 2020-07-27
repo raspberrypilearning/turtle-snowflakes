6349e2bf32b2063e31e06d347076386b2dc3b642
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-07-27 17:25:40 +0100

    New translations snowflake.py (Kannada)

--- kn-IN/resources/snowflake.py
+++ kn-IN/resources/snowflake.py
@@ -4,13 +4,13 @@ import turtle
 import random
 elsa = turtle.Turtle()
 
-turtle.Screen().bgcolor("grey")
+turtle.Screen().bgcolor("blue")
 colours = ["cyan", "purple", "white", "blue"]
 
-elsa.penup()
+elsa.penup ()
 elsa.forward(90)
-elsa.left(45)
-elsa.pendown()
+elsa.left (45)
+elsa.pendown ()
 
 def branch():
   for i in range(3):
@@ -20,12 +20,12 @@ def branch():
       elsa.right(45)
     elsa.left(90)
     elsa.backward(30)
-    elsa.left(45)
+    elsa.left (45)
   elsa.right(90)
   elsa.forward(90)
 
 for i in range(8):
     branch()
-    elsa.left(45)
+    elsa.left (45)
 
 # elsa.color(random.choice(colours))
