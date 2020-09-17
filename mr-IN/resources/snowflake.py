7cac16997b5c002fc004ae8d7bcf4dc9bf812bbd
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-09-17 13:11:36 +0100

    Add translations
    Marathi

---
+++ mr-IN/resources/snowflake.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import turtle
+import random
+elsa = turtle.Turtle()
+
+turtle.Screen().bgcolor("grey")
+colours = ["cyan", "purple", "white", "blue"]
+
+elsa.penup()
+elsa.forward(90)
+esa.left (45)
+elsa.pendown()
+
+def branch():
+  for i in range(3):
+    for i in range(3):
+      elsa.forward(30)
+      elsa.backward(30)
+      elsa.right(45)
+    elsa.left(90)
+    elsa.backward(30)
+    elsa.left(45)
+  elsa.right(90)
+  elsa.forward(90)
+
+for i in range(8):
+    branch()
+    elsa.left(45)
+
+# elsa.color(random.choice(colours))
